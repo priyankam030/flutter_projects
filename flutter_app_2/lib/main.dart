7e28f6ade803d254fef9a5b24f22e8bbf88eb141
priyankam030 <priyankamourya030@gmail.com>
2024-08-25 00:06:21 +0530

    added text size and text color

--- flutter_app_2/lib/main.dart
+++ flutter_app_2/lib/main.dart
@@ -5,15 +5,23 @@ void main() {
     MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
-              const Color.fromARGB(255, 77, 214, 161),
-              const Color.fromARGB(255, 234, 234, 54),
-              const Color.fromARGB(255, 203, 131, 55)]
-            )
+              Color.fromARGB(255, 77, 214, 161),
+              Color.fromARGB(255, 234, 234, 54),
+              Color.fromARGB(255, 203, 131, 55), 
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
-          child:const Center(child: Text("Welcome Adnroid world.."),
+          child:const Center(child: Text("Welcome Adnroid world..",
+          style: TextStyle(
+            color: Color.fromARGB(255, 250, 248, 244),
+            fontSize: 28.00,
+          ),
+          ),
           ),
         ),
       ),
