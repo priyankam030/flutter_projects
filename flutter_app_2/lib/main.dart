ff933b06252233e5979517e00222c886730d246d
priyankam030 <priyankamourya030@gmail.com>
2024-08-24 22:19:07 +0530

    added background graident color

--- flutter_app_2/lib/main.dart
+++ flutter_app_2/lib/main.dart
@@ -2,10 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 30, 235, 12),
-        body: Center(child: Text("Welcome Adnroid world.."),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+              const Color.fromARGB(255, 77, 214, 161),
+              const Color.fromARGB(255, 234, 234, 54),
+              const Color.fromARGB(255, 203, 131, 55)]
+            )
+          ),
+          child:const Center(child: Text("Welcome Adnroid world.."),
+          ),
         ),
       ),
     ),
