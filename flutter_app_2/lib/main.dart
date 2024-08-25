bcf867b40e477eceab87603a5943995e050c2490
priyankam030 <priyankamourya030@gmail.com>
2024-08-25 19:20:46 +0530

    added costom widget

--- flutter_app_2/lib/main.dart
+++ flutter_app_2/lib/main.dart
@@ -4,27 +4,37 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-              Color.fromARGB(255, 77, 214, 161),
-              Color.fromARGB(255, 234, 234, 54),
-              Color.fromARGB(255, 203, 131, 55), 
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child:const Center(child: Text("Welcome Adnroid world..",
-          style: TextStyle(
-            color: Color.fromARGB(255, 250, 248, 244),
-            fontSize: 28.00,
-          ),
-          ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+
+  @override
+  Widget build(context)
+  {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+          Color.fromARGB(255, 77, 214, 161),
+          Color.fromARGB(255, 234, 234, 54),
+          Color.fromARGB(255, 203, 131, 55), 
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child:const Center(
+        child: Text("Adnroid world....", 
+          style: TextStyle(
+            color: Color.fromARGB(255, 140, 5, 185),
+            fontSize: 28.00,
+          ),
+        ),
+      ),
+    );
+  }
 }
