6b4f7ac0fe2b8cb923b57621a35c070a449b336b
priyankam030 <priyankamourya030@gmail.com>
2024-09-10 23:07:10 +0530

    same thing added if statement method

--- flutter_app_3/lib/quiz.dart
+++ flutter_app_3/lib/quiz.dart
@@ -1,32 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_3/questions_screen.dart';
 import 'package:flutter_app_3/start_screen.dart';
 
 class Quiz extends StatefulWidget {
- const Quiz({super.key});
+  const Quiz({super.key});
   @override
   State<Quiz> createState() {
-    return _quizstate();
-  
+    return _QuizState();
   }
 }
 
-class _quizstate extends State<Quiz>{
- 
- @override
- Widget build(context){
-  return MaterialApp(
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen();
+    }
+
+    screenWidget = const QuestionsScreen();
+
+    return MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration: const BoxDecoration(gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 95, 44, 224),
-              Color.fromARGB(255, 65, 109, 186)
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 95, 44, 224),
+                Color.fromARGB(255, 65, 109, 186)
               ],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
             ),
           ),
-          child: const StartScreen(),
+          child: screenWidget,
         ),
       ),
     );
